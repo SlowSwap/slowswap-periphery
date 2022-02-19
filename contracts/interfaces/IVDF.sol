d196b1fdf1925244d323ee46a9b20abdce51575d
Lawrence Forman <me@merklejerk.com>
2022-02-19 04:02:20 -0700

    again with the pragma

--- contracts/interfaces/IVDF.sol
+++ contracts/interfaces/IVDF.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^ 0.8;
+pragma solidity ^0.8;
 
 interface IVDF {
     function isValidProof(bytes32 seed, bytes calldata proofBytes) external returns (bool);
